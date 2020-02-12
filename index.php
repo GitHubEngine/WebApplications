3b04f5f93ed1f2c395ac467699a74e9c99765d53
IgorMuramshchikov <igor.muram@gmail.com>
2020-02-12 20:35:29 +0300

    Create news

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+//require("functions.php"); 
+//connectDB();
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<?php
+require("php/blocks/head.php");
+?>
+
+<body>
+	<?php
+	require("php/blocks/preloader.php");
+	?>
+
+	<div class="container">
+		<?php
+		require("php/blocks/header.php");
+		require("php/index/index-main.php");
+		require("php/blocks/footer.php");
+		?>
+	</div>
+
+
+	<script src="js/scripts.min.js"></script>
+</body>
+</html>
